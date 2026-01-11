f685ff6fb897d67d562f54f128c7125bcac0754a
GrishaYak <gishayak2@gmail.com>
2026-01-11 16:19:57 +0500

    It worked!

--- UI/Preferences.cs
+++ UI/Preferences.cs
@@ -1,4 +1,3 @@
-using System.Reflection.Metadata;
 using Microsoft.Xna.Framework;
 using static PingPong.Window;
 
@@ -8,12 +7,12 @@ public static class Preferences
 {
     public static class Font
     {
-        public static string FontFile { get; } = "fonts/default.fnt";
-        public static float StandardFontScale { get; } = SymbolScreenHeightScale * 0.055f;
+        public const string FontFile = "fonts/default.fnt";
+        public const float StandardFontScale = SymbolScreenHeightScale * 0.055f;
     }
     public static class UniversalTextures
     {
-        public static string Transparent { get; } = "transparent";
+        public const string Transparent = "transparent";
     }
     public static class Colors
     {
@@ -23,51 +22,51 @@ public static class Preferences
     }
     public static class Button
     {
-        public static float Width { get; } = BufferWidth * 0.15f;
-        public static float Height { get; } = BufferHeight * 0.095f;
-        public static float TextIndent { get; } = BufferMultiplyer * 4;
-        public static string UnfocusedRegion { get; } = "unfocused_btn";
-        public static string FocusedRegion { get; } = "focused_btn";
-        public static float StandardSizeMultiplyer { get; } = 1;
-        public static float StandardFontScale { get; } = Font.StandardFontScale;
-        public static string FontFile { get => Font.FontFile; }
+        public const float Width = BufferWidth * 0.15f;
+        public const float Height = BufferHeight * 0.095f;
+        public const float TextIndent = BufferMultiplyer * 4;
+        public const string UnfocusedRegion = "unfocused_btn";
+        public const string FocusedRegion = "focused_btn";
+        public const float StandardSizeMultiplyer = 1;
+        public const float StandardFontScale = Font.StandardFontScale;
+        public const string FontFile = Font.FontFile;
     }
     public static class SliderWithText
     {
-        public static float StandardIndent { get; } = TotalHeight + BufferHeight * 0.05f;
-        public static float TotalWidth { get; } = 0.7f * BufferWidth;
-        public static float TotalHeight { get; } = 0.35f * BufferHeight;
-        public static float TextX { get; } = 0;
-        public static float TextY { get; } = 0;
+        public const float StandardIndent = TotalHeight + BufferHeight * 0.05f;
+        public const float TotalWidth = 0.7f * BufferWidth;
+        public const float TotalHeight = 0.35f * BufferHeight;
+        public const float TextX = 0;
+        public const float TextY = 0;
         public static class SliderOnly
         {
-            public static float Width { get; } = TotalWidth;
-            public static float Height { get; } = TotalHeight * 0.65f;
-            public static float X { get; } = 0f;
-            public static float Y { get; } = GetFontSize(FontScale) + 0.01f * BufferHeight;
+            public const float Width = TotalWidth;
+            public const float Height = TotalHeight * 0.65f;
+            public const float X = 0f;
+            public const float Y = FontScale * FontRatio + 0.01f * BufferHeight;
 
         }
-        public static string LeftSliderRegion { get; } = "slider-off";
-        public static string MiddleSliderRegion { get; } = "slider-middle";
-        public static string RightSliderRegion { get; } = "slider-max";
-        public static float FontScale { get; } = Font.StandardFontScale * 1.05f;
-        public static string FontFile { get; } = Font.FontFile;
-        public static float LeftSliderPartWidth { get; } = BufferWidth * 0.09f;
-        public static float RightSliderPartWidth { get; } = LeftSliderPartWidth;
-        public static float MiddleSliderPartWidth { get; } = SliderOnly.Width - LeftSliderPartWidth - RightSliderPartWidth;
+        public const string LeftSliderRegion = "slider-off";
+        public const string MiddleSliderRegion = "slider-middle";
+        public const string RightSliderRegion = "slider-max";
+        public const float FontScale = Font.StandardFontScale * 1.05f;
+        public const string FontFile = Font.FontFile;
+        public const float LeftSliderPartWidth = BufferWidth * 0.09f;
+        public const float RightSliderPartWidth = LeftSliderPartWidth;
+        public const float MiddleSliderPartWidth = SliderOnly.Width - LeftSliderPartWidth - RightSliderPartWidth;
         /// <summary>
         /// Value of slider in percents.
         /// </summary>
-        public static float DefaultSliderValue { get; } = 100f;
+        public const float DefaultSliderValue = 100f;
     }
 
     public static class OptionsPanel
     {
         public static class OptionsText
         {
-            public static float X {get;} = BufferWidth * 0.05f;
-            public static float Y {get;} = BufferHeight * 0.05f;
-            public static float FontScale {get;} = Font.StandardFontScale * 2f;
+            public const float X = BufferWidth * 0.05f;
+            public const float Y = BufferHeight * 0.05f;
+            public const float FontScale = Font.StandardFontScale * 2f;
 
         }
     }
--- Window.cs
+++ Window.cs
@@ -2,29 +2,29 @@ namespace PingPong;
 
 public static class Window
 {
-    public static string WindowName { get; } = "AeroPong";
-    public static int ScreenWidth { get; } = 900;
-    public static int ScreenHeight { get; } = 500;
-    public static bool IsFullScreen { get; } = false;
-    public static float BufferMultiplyer { get; } = 3.0f;
-    public static int BufferWidth { get => ScreenWidth / (int)BufferMultiplyer; }
-    public static int BufferHeight { get => ScreenHeight / (int)BufferMultiplyer; }
+    public const string WindowName  = "AeroPong";
+    public const int ScreenWidth  = 900;
+    public const int ScreenHeight  = 500;
+    public const bool IsFullScreen  = false;
+    public const float BufferMultiplyer  = 3.0f;
+    public const int BufferWidth  = ScreenWidth / (int)BufferMultiplyer; 
+    public const int BufferHeight = ScreenHeight / (int)BufferMultiplyer; 
 
     /// <summary>
     /// A constant you have to multiply on to make a symbol have a height of a screen
     /// </summary>
-    public static float SymbolScreenHeightScale { get; } = BufferHeight / 26;
+    public const float SymbolScreenHeightScale  = BufferHeight / 26;
 
-    private static readonly float ratio = BufferHeight / SymbolScreenHeightScale;
+    public const float FontRatio = BufferHeight / SymbolScreenHeightScale;
 
     /// <summary>
-    /// Returns size of a letter in pixels in buffer.
+    /// Returns height of a letter in pixels. (simply multiplies given fontScale with FontRatio)
     /// </summary>
     /// <param name="fontScale"></param>
     /// <returns></returns>
     public static float GetFontSize(float fontScale)
     {
-        return fontScale * ratio;
+        return fontScale * FontRatio;
     }
 
 }
